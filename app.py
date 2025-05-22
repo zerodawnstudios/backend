82ef05db121396ec31ec58d644bc0df4d2a6b52c
M-Ali-Haider <mah01134212100@gmail.com>
2025-05-22 08:39:16 +0500

    added frontend url for CORS

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000', "https://fyp-frontend-lake.vercel.app"])
+CORS(app, origins=['http://localhost:3000', "https://smarthome-six-sigma.vercel.app"])
 
 def create_keyfile_dict():
     variables_keys = {
